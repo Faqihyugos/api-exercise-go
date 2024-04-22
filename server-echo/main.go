a79b04e46e74caca49181f98cf0637ad2627d51e
Faqihyugos <faqihyugos@gmail.com>
2024-04-22 17:03:13 +0700

    add json example for simple echo

---
+++ server-echo/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// go mod init <url>/<package>
+// go mod init github.com/<user>/<repo>
+
+func main() {
+	e := echo.New()
+	e.POST("/", postHandler)
+	e.GET("/", getHandler)
+	e.Start(":8080")
+}
+func postHandler(c echo.Context) error {
+	c.String(200, "hello world from echo, method: POST")
+	return nil
+}
+
+func getHandler(c echo.Context) error {
+	//c.String(200, "hello world from echo")
+	c.JSON(http.StatusOK, map[string]string{
+		"name":  "john snow",
+		"house": "stark",
+	})
+	return nil
+}
